e28ca91765a28df378edcf34b38aa067d90a3ca5
Bernardo Reis <bernardomreis19@gmail.com>
2024-04-23 10:19:24 +0100

    Create project

---
+++ src/main/java/com/example/spring_mongo_first/SpringMongoFirstApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_mongo_first;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMongoFirstApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMongoFirstApplication.class, args);
+	}
+
+}
