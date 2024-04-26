7dc90d68d2654f91a49d923d6fdf860a645d2bc0
Bernardo Reis <bernardomreis19@gmail.com>
2024-04-26 17:42:43 +0100

    AuthorDTO to projection data

--- src/main/java/com/example/spring_mongo_first/config/Instantiation.java
+++ src/main/java/com/example/spring_mongo_first/config/Instantiation.java
@@ -9,6 +9,7 @@ import org.springframework.context.annotation.Configuration;
 
 import com.example.spring_mongo_first.domain.Post;
 import com.example.spring_mongo_first.domain.User;
+import com.example.spring_mongo_first.dto.AuthorDTO;
 import com.example.spring_mongo_first.repository.PostRepository;
 import com.example.spring_mongo_first.repository.UserRepository;
 
@@ -32,9 +33,12 @@ public class Instantiation implements CommandLineRunner{
 		User alex = new User(null, "Alex Green", "alex@gmail.com"); 
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
 		
-		Post post1 = new Post(null, "Trip to Brasil", "I am going to Brasil next week!", sdf.parse("2023/02/24"), maria);
-		Post post2 = new Post(null, "Good morning", "Wish a great day for everyone", sdf.parse("2023/07/08"), maria);
+		//Save users first to associate them with posts later, if users are not saved post will return author id NULL
 		userRepo.saveAll(Arrays.asList(maria, alex, bob));
+		
+		Post post1 = new Post(null, "Trip to Brasil", "I am going to Brasil next week!", sdf.parse("2023/02/24"), new AuthorDTO(maria));
+		Post post2 = new Post(null, "Good morning", "Wish a great day for everyone", sdf.parse("2023/07/08"),new AuthorDTO(maria));
+		
 		postRepo.saveAll(Arrays.asList(post1, post2));
 	}
 
--- src/main/java/com/example/spring_mongo_first/domain/Post.java
+++ src/main/java/com/example/spring_mongo_first/domain/Post.java
@@ -7,6 +7,8 @@ import java.util.Objects;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import com.example.spring_mongo_first.dto.AuthorDTO;
+
 @Document
 public class Post implements Serializable{
 	
@@ -16,13 +18,13 @@ public class Post implements Serializable{
 	private String title;
 	private String body;
 	private Date date;
-	private User author;
+	private AuthorDTO author;
 	
 	public Post() {
 		
 	}
 
-	public Post(String id, String title, String body, Date date, User author) {
+	public Post(String id, String title, String body, Date date, AuthorDTO author) {
 		this.id = id;
 		this.title = title;
 		this.body = body;
@@ -62,11 +64,11 @@ public class Post implements Serializable{
 		this.date = date;
 	}
 	
-	public User getAuthor() {
+	public AuthorDTO getAuthor() {
 		return author;
 	}
 
-	public void setAuthor(User author) {
+	public void setAuthor(AuthorDTO author) {
 		this.author = author;
 	}
 
