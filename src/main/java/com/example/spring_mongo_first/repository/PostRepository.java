0bbae3fbf7853b1244c50ea79b664011be701083
Bernardo Reis <bernardomreis19@gmail.com>
2024-04-30 16:08:42 +0100

    Query method with anotation @Query

--- src/main/java/com/example/spring_mongo_first/repository/PostRepository.java
+++ src/main/java/com/example/spring_mongo_first/repository/PostRepository.java
@@ -3,6 +3,7 @@ package com.example.spring_mongo_first.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.example.spring_mongo_first.domain.Post;
@@ -12,4 +13,9 @@ public interface PostRepository extends MongoRepository<Post, String>{
 	
 	//to be case insensitive and ignore case use method findByTitleContainingIgnoreCase
 	List<Post> findByTitleContaining(String text);
+	
+	//to be case insensitive and ignore case use { 'title': { $regex: ?0, $options: 'i' } }
+	@Query("{ 'title': { $regex: ?0 } }")
+	List<Post> findByTitleFilter(String text);
+	
 }
