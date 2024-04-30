17a2abee9e270d51d80d5ffd5cac2be8747fe9fa
Bernardo Reis <bernardomreis19@gmail.com>
2024-04-30 12:00:18 +0100

    Add comments to posts

--- src/main/java/com/example/spring_mongo_first/config/Instantiation.java
+++ src/main/java/com/example/spring_mongo_first/config/Instantiation.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
 import com.example.spring_mongo_first.domain.Post;
 import com.example.spring_mongo_first.domain.User;
 import com.example.spring_mongo_first.dto.AuthorDTO;
+import com.example.spring_mongo_first.dto.CommentDTO;
 import com.example.spring_mongo_first.repository.PostRepository;
 import com.example.spring_mongo_first.repository.UserRepository;
 
@@ -36,8 +37,16 @@ public class Instantiation implements CommandLineRunner{
 		//Save users first to associate them with posts later, if users are not saved post will return author id NULL
 		userRepo.saveAll(Arrays.asList(maria, alex, bob));
 		
-		Post post1 = new Post(null, "Trip to Brasil", "I am going to Brasil next week!", sdf.parse("2023/02/24"), new AuthorDTO(maria));
-		Post post2 = new Post(null, "Good morning", "Wish a great day for everyone", sdf.parse("2023/07/08"),new AuthorDTO(maria));
+		Post post1 = new Post(null, "Trip to Brasil", "I am going to Brasil next week!", sdf.parse("24/02/2023"), new AuthorDTO(maria));
+		Post post2 = new Post(null, "Good morning", "Wish a great day for everyone", sdf.parse("08/07/2023"), new AuthorDTO(maria));
+		
+		CommentDTO comment1 = new CommentDTO("Have a nice trip", sdf.parse("11/03/2023"), new AuthorDTO(alex));
+		CommentDTO comment2 = new CommentDTO("Enjoy!", sdf.parse("05/04/2023"), new AuthorDTO(bob));
+		CommentDTO comment3 = new CommentDTO("Thank you, you too!", sdf.parse("08/07/2023"), new AuthorDTO(alex));
+		
+		post1.getComments().add(comment1);
+		post1.getComments().add(comment2);
+		post2.getComments().add(comment3);
 		
 		postRepo.saveAll(Arrays.asList(post1, post2));
 		
